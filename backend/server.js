0c658c41cab64006abcef7e587b5853ebaba11b6
Shivavarmavanaparthi <your-email@example.com>
2026-03-27 15:53:28 +0530

    fix: regex catch-all for node 22 compatibility

--- backend/server.js
+++ backend/server.js
@@ -38,7 +38,7 @@ app.use("/api/payments/",paymentRoute);
 if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"/frontend/dist")));
 
-  app.get("*",(req, res) => {
+  app.get(/^(?!\/api).+/,(req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
   
