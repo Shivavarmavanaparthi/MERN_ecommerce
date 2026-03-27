7ebb3c3c7425fcd5961ec26797c8e8552b43ffc1
Shivavarmavanaparthi <your-email@example.com>
2026-03-27 14:28:15 +0530

    feat: handle client-side routing and static files for production

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,9 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"/frontend/dist")));
 }
 
+app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
 
 app.listen(PORT,()=>{
   console.log("Server is running on http://localhost:"+PORT);
