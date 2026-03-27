0b3b3edcfd16b7238ff006e294f5363826ce5b76
Shivavarmavanaparthi <your-email@example.com>
2026-03-27 15:04:38 +0530

    fix: final attempt at express v5 route syntax

--- backend/server.js
+++ backend/server.js
@@ -39,7 +39,7 @@ if(process.env.NODE_ENV==="production"){
   app.use(express.static(path.join(__dirname,"/frontend/dist")));
 }
 
-app.get("/:index*", (req, res) => {
+app.get("/*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
 
